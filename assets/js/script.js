a1a1762ad57c597986ada129636359abd4ff75ef
Narwhrl <1139207303@qq,com>
2023-03-10 11:55:18 +0800

    final-dev-commnit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,58 @@
 'use strict';
 
+/**
+ * test
+ */
+
+function changeImg(img, hoverImgSrc) {
+  // 创建一个新的Image对象，用于预加载hover图片
+  var hoverImg = new Image();
+  hoverImg.src = hoverImgSrc;
+  
+  // 渐变效果
+  var fadeOut = setInterval(function() {
+    if (img.style.opacity > 0) {
+      img.style.opacity -= 0.1;
+    } else {
+      clearInterval(fadeOut);
+      img.src = hoverImgSrc;
+      var fadeIn = setInterval(function() {
+        if (img.style.opacity < 1) {
+          img.style.opacity = parseFloat(img.style.opacity) + 0.1;
+        } else {
+          clearInterval(fadeIn);
+        }
+      }, 50);
+    }
+  }, 50);
+}
+
+
+/**
+var socialIcons = document.querySelectorAll('.social-icon');
+
+for (var i = 0; i < socialIcons.length; i++) {
+  socialIcons[i].addEventListener('mouseover', function() {
+    var iconId = this.getAttribute('id');
+    var secondImagePath = './assets/images/' + iconId + '-hover.png';
+    setTimeout(function() {
+      this.style.transition = 'opacity 0.3s ease-in-out';
+      this.style.opacity = '0.5';
+    }.bind(this), 100);
+    this.setAttribute('src', secondImagePath);
+  });
+
+  socialIcons[i].addEventListener('mouseout', function() {
+    var iconId = this.getAttribute('id');
+    var firstImagePath = './assets/images/' + iconId + '.png';
+    setTimeout(function() {
+      this.style.transition = 'opacity 0.3s ease-in-out';
+      this.style.opacity = '1';
+    }.bind(this), 100);
+    this.setAttribute('src', firstImagePath);
+  });
+}
+*/
 
 
 /**
